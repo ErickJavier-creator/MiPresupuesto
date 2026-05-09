7aacc3b6abf363ffece5286789d75bac3a839bd1
Erick Javier <javawdev@gmail.com>
2026-05-09 18:12:28 -0400

    asd

--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 export function HomeTemplate() {
   return (<Container>
-<h1>Bienvendio home</h1>
+<h1>Bienvendio homess</h1>
   </Container>);
 }
 const Container =styled.div`
