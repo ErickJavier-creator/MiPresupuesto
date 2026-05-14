e036903c22d2b379373c98099dd426d5a29669b9
Erick Javier <javawdev@gmail.com>
2026-05-14 11:45:29 -0400

    Refactor AuthContext to improve user session management and integrate new Supabase functions for user data handling. Update imports and enhance user state persistence upon authentication state changes.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,42 +1,38 @@
-import {createContext, useContext, useEffect, useState} from "react"
-import {supabase, insertarUsuarios} from "../index";
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase,InsertarUsuarios } from "../index";
 const AuthContext = createContext();
-export const AuthContextProvider = ({children}) => {
-    const [user, setUser] = useState([]);
-    useEffect(()=>{
-        const {data: authListener} = supabase.auth.onAuthStateChange(
-            async (event, session) =>{
-                if(session==null){
-                    setUser(null)
-                }else {
-                    setUser(session?.user.user_metadata)
-                 await persistirUsuarioSesion(
-                   session?.user.user_metadata,
-                   session?.user.id,
-                 );
-                    console.log("session: ",session?.user.user_metadata)
-                    console.log("event:", event)
-                }
-            }
-        )
-        return () => {
-            authListener.subscription;
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        if (session == null) {
+          setUser(null);
+        } else {
+          setUser(session?.user.user_metadata);
+          insertarUsuarios(session?.user.user_metadata,session?.user.id);
+          console.log("event", event);
+          console.log("session", session?.user.user_metadata);
         }
-    }, []);
-    const persistirUsuarioSesion = async (dataProvider, idAuthSupabase) => {
-        const p = {
-            nombres: dataProvider.name,
-            foto: dataProvider.picture,
-            idauth_supabase: idAuthSupabase,
-        };
-        await insertarUsuarios(p);
-    };
-    return (
-      <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+      }
     );
+    return () => {
+      authListener.subscription;
+    };
+  }, []);
+  const insertarUsuarios = async (dataProvider, idAuthSupabase) => {
+    const p = {
+      nombre: dataProvider.name,
+      foto: dataProvider.picture,
+      idauth_supabase: idAuthSupabase,
+    };
+    const result = await InsertarUsuarios(p);
 
-}
-
-export const UserAuth=() => {
-    return useContext(AuthContext)
-}
+  };
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
--- src/supabase/crudUsuarios.jsx
+++ src/supabase/crudUsuarios.jsx
@@ -1,10 +1,9 @@
-import { supabase } from "./supabase.config";
-
-export const InsertarUsuario = async (p) => {
-    try {
-      const {data} =  await supabase.from("usuarios").insert(p).select();
-        return data;
-    } catch (error) {
-
-    }
-}
+import { supabase } from "../index";
+export const InsertarUsuarios = async (p) => {
+  try {
+    const { data } = await supabase.from("usuarios").upsert([p], {
+      onConflict: "idauth_supabase",
+    }).select();
+    return data;
+  } catch (error) {}
+};
